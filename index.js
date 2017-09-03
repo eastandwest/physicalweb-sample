f3e8fa8dc71acc48036ce1680dac48226f52143c
kensaku komatsu <kensaku.komatsu@gmail.com>
2017-09-02 20:49:06 -0700

    1st commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const ip = require('ip')
+const https = require('https')
+const fs = require('fs')
+const path = require('path')
+const eddystoneBeacon = require('eddystone-beacon')
+const express = require('express')
+
+const key = fs.readFileSync(path.join( __dirname + "/self-signed/server.key"))
+const cert = fs.readFileSync(path.join( __dirname + "/self-signed/server.crt"))
+
+const app = express()
+app.get('/', (req, res) => {
+  res.send('it works!!')
+})
+
+const port = process.env.PORT || 3000
+const httpsServer = https.createServer({key, cert}, app)
+httpsServer.listen(port, () => {
+  try {
+    console.log(`express server started on port ${port}`)
+    const url = `https://${ip.address()}:${port}`
+    eddystoneBeacon.advertiseUrl(url)
+    console.log(`broadcasting url : ${url} by physical web`)
+  } catch(err) {
+    console.warn(err.message)
+  }
+})
